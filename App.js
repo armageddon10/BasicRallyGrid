49d63904f42768a19e52d06ce49bce1c985b13eb
Sayak Biswas <sayak90@gmail.com>
2014-09-27 21:16:36 +0000

    working grid v1.1

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
-    items:{ html:'<a href="https://help.rallydev.com/apps/2.0rc3/doc/">App SDK 2.0rc3 Docs</a>'},
+    items:{ 
     launch: function() {
         var myStore = Ext.create('Rally.data.wsapi.Store', {
             model: 'User Story',
